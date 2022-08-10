dd141548ceec9b3d739210062cf650973342ad5b
Calvin Koo <kkoo22@students.claremontmckenna.edu>
2022-08-10 10:50:41 -0400

    sessions actions and reducer

---
+++ frontend/actions/session_actions.js
@@ -0,0 +1,49 @@
+import { postUser, postSession, deleteSession } from '../util/session_api_util';
+
+export const RECEIVE_CURRENT_USER = 'RECEIVE_CURRENT_USER';
+export const LOGOUT_CURRENT_USER = 'LOGOUT_CURRENT_USER';
+export const RECEIVE_SESSION_ERRORS = 'RECEIVE_SESSION_ERRORS';
+export const REMOVE_SESSION_ERRORS = 'REMOVE_SESSION_ERRORS';
+
+//regular action creators
+export const receiveCurrentUser = (currentUser) => {
+    return {
+        type: RECEIVE_CURRENT_USER,
+        currentUser
+    };
+};
+
+export const logoutCurrentUser = () => {
+    return {
+        type: LOGOUT_CURRENT_USER,
+    };
+};
+
+export const receiveErrors = (errors) => {
+    return {
+        type: RECEIVE_SESSION_ERRORS,
+        errors
+    };
+};
+
+export const deleteErrors = () => {
+    return {
+        type: REMOVE_SESSION_ERRORS
+    };
+};
+
+
+//thunk action creators
+
+//denoting as formUser to not get confused
+    //we are getting this user from the form the a user fills out
+export const signup = formUser = dispatch => postUser(formUser)
+    .then(user => dispatch(receiveCurrentUser(user)),
+    errors => dispatch(receiveErrors(errors.responseJSON)));
+
+export const login = formUser = dispatch => postSession(formUser)
+    .then(user => dispatch(receiveCurrentUser(user)),
+    errors => dispatch(receiveErrors(errors.responseJSON)));
+
+export const logout = () => dispatch => deleteSession()
+    .then(() => dispatch(logoutCurrentUser()));
---
+++ frontend/reducers/session_reducer.js
@@ -0,0 +1,28 @@
+import { RECEIVE_CURRENT_USER, LOGOUT_CURRENT_USER } from "../actions/session_actions";
+
+//create default state for session
+const _nullSession = {
+    currentUser: null
+    // potentially fix logic to make the session slice of state refer to profile as key
+};
+
+const sessionReducer = (state = _nullUser, action) => {
+    Object.freeze(state);
+
+    switch(action.type) {
+        case RECEIVE_CURRENT_USER:
+            return Object.assign({}, {currentUser: action.currentUser.id})
+        case LOGOUT_CURRENT_USER:
+            return _nullSession;
+        default:
+            return state;
+
+    }
+}
+
+export default sessionReducer;
+
+
+
+
+
