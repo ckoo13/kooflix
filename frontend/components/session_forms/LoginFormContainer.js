957b0f695c7ac9019342d127dd386acf2789c4f1
Calvin Koo <kkoo22@students.claremontmckenna.edu>
2022-08-15 16:28:20 -0400

    heroku

--- frontend/components/session_forms/LoginFormContainer.js
+++ frontend/components/session_forms/LoginFormContainer.js
@@ -2,21 +2,30 @@ import React from "react";
 import { connect } from "react-redux";
 import { login, deleteErrors } from "../../actions/session_actions";
 import { Link } from "react-router-dom";
-import SessionForm from "./SessionForm";
+// import SessionForm from "./SessionForm";
 
-const mapStateToProps = (state, ownProps) => {
-    return {
-        errors: state.errors.session,
-        formType: 'Log In',
-        navLink: <Link id="session-redirect-link" to={'/signup'}>Sign up now.</Link>
-    }
-};
+// const mapStateToProps = (state, ownProps) => {
+//     return {
+//         errors: state.errors.session,
+//         formType: 'Log In',
+//         navLink: <Link id="session-redirect-link" to={'/signup'}>Sign up now.</Link>
+//     }
+// };
 
-const mapDispatchToProps = (dispatch, ownProps) => {
-    return {
-        processForm: user => dispatch(login(user)),
-        deleteErrors: () => dispatch(deleteErrors())
-    }
-};
+// const mapDispatchToProps = (dispatch, ownProps) => {
+//     return {
+//         processForm: user => dispatch(login(user)),
+//         deleteErrors: () => dispatch(deleteErrors())
+//     }
+// };
 
-export default connect(mapStateToProps, mapDispatchToProps)(SessionForm)
+
+const loginFormContainer = () => {
+  return (
+    <div>loginFormContainer</div>
+  )
+}
+
+export default loginFormContainer
+
+// export default connect(mapStateToProps, mapDispatchToProps)(SessionForm)
