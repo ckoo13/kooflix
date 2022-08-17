9b684e0352b207f1c853ddcedd433d61811e2953
Calvin Koo <kkoo22@students.claremontmckenna.edu>
2022-08-17 18:39:15 -0400

    profiles index almost done

--- frontend/components/profile/ProfileIndex.jsx
+++ frontend/components/profile/ProfileIndex.jsx
@@ -11,12 +11,50 @@ class ProfileIndex extends React.Component {
         this.props.getProfiles(this.props.currentUser);
     }
 
-    render() {
-        return (
-            <div>
-                <h1>Profiles</h1>
-                <NavBarContainer />
+    renderProfiles() {
+        const { profiles } = this.props
 
+        return (
+            profiles.map((profile) => {
+                return (
+                    <li key={profile.id}>
+                        <div>
+                            <Link className="profile-image-link" to={'/browse'}>
+                                <div className="avatar-wrapper">
+                                    <img className="profile-image" src={profileURL} alt="" />
+                                    <p className="profile-image-name">{profile.name}</p>
+                                </div>
+                             </Link>
+                        </div>
+                    </li>
+                )
+            })
+        )
+    }
+
+    render() {
+        const renderProfiles = this.renderProfiles()
+
+        return (
+            <div className="profiles-index-main-container">
+                <div className="profiles-header">
+                    <Link to={'/browse'}>
+                        <img className="profiles-splash-logo" src={logoUrl} alt="" />
+                    </Link>
+                </div>
+                <div className="profiles-gate-container">
+                    <div className="profiles-index-container">
+                        <h1>Who's watching?</h1>
+                        <div className="profiles-images-container">
+                            <ul className="profiles-images-list">
+                                {renderProfiles} 
+                            </ul>
+                        </div>
+                        <button id="manage-profiles-button">Manage Profiles</button>
+                    </div>
+                </div>
+                {/* delete later this is the logout button */}
+                <NavBarContainer />
             </div>
         )
     }
