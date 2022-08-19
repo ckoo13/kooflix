471567dc3331756691954142442cacc7bfaa1299
Calvin Koo <kkoo22@students.claremontmckenna.edu>
2022-08-19 11:06:42 -0400

    gonna have to make the components for videos onw

--- frontend/components/browse/Browse.jsx
+++ frontend/components/browse/Browse.jsx
@@ -8,15 +8,15 @@ class Browse extends React.Component {
     }
 
     componentDidMount() {
-      this.props.getVideos();
+      this.props.getVideos()
+        .then(() => {document.getElementById('test').src = this.props.video.thumbnailUrl})
     }
 
     render() {
       return (
           <div>
               <h1>Main Page</h1>
-              {/* <img src={this.props.video.thumbnailUrl} alt="" /> */}
-              {console.log(this.props.video)}
+              <img id='test' src='' alt="" />
               <NavbarContainer />
           </div>
         )
