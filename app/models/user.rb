cf2ffedb2fbe5dcae0137d81bf775d32b9491e5a
Calvin Koo <kkoo22@students.claremontmckenna.edu>
2022-09-02 20:41:25 -0700

    user model fix

--- app/models/user.rb
+++ app/models/user.rb
@@ -18,7 +18,7 @@ class User < ApplicationRecord
     #Validations
     validates :email, :session_token, :password_digest, presence: true
     validates :email, :session_token, uniqueness: true
-    validates :password, length: {minimum: 6, allow_nil: true}
+    validates :password, length: {minimum: 6}, allow_nil: true
 
     attr_reader :password
 
